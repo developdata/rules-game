0b890449641c64e67eab72ee28fa61c6efe4f7a6
developdata <isk9@hotmail.com>
2020-10-10 16:20:37 +0100

    cleaned up code and changed varialbe names tto match the project

--- index.js
+++ index.js
@@ -6,97 +6,20 @@ var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
-var rulesArray = [];
-
 app.use(express.static('public'));
 
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-// io.on('connection', function(socket) {
-// 	console.log(socket.id);
-
-// 	// socket.on('new rules', function(rules){
-// 	// 	console.log(rules);
-// 	// 	rulesArray = rulesArray.concat(rules);
-// 	// 	console.log(rulesArray);
-
-// 	// 	socket.emit('all rules', rulesArray);
-// 	// 	console.log('emited')
-// 	// });
-
-// 	socket.on('disconnect', function() {
-// 		console.log(socket.id);
-// 	});
-
-// });
-
-var players = {};
-
-var max = 300;
-var min = 2;
-
-
-//When a browser page connects to the server the function
-//io.on is called with the name connection. The function contains
-//information about the new socket including an id
 io.on('connection', function(socket) {
-  
-  // console.log(socket)
-  
-  //When a person connects to the game an socket id is created,
-  //An new object is added to the object players for that player
-  //A random x and y number is generated between 1 and 300
-  //This is used for the intitial position of the circle
-  //The name the person typed in is also added to their object
-  socket.on('new player', function(d) {
-  	console.log(d)
-    // players[socket.id] = {
-    //   x: Math.floor(Math.random() * (max - min + 1) + min),
-    //   y: Math.floor(Math.random() * (max - min + 1) + min),
-    //   name: d
-    // };
 
-    
-    // let nameArray = [];
-    // Object.keys(players).forEach(key => {
-    //   nameArray.push(players[key].name);
-    // });
-    
-    io.sockets.emit('update player names', d);
-    
+  socket.on('new rules', function(d) {
+    io.sockets.emit('update rules', d);
   });
   
    socket.on('disconnect', function() {
-     
-     console.log(socket)
-
-    delete players[socket.id];
-     
-    let nameArray = [];
-    Object.keys(players).forEach(key => {
-      nameArray.push(players[key].name);
-    });
-     
-      io.sockets.emit('update player names', nameArray);
-     
-  });
-  
-  socket.on('movement', function(data) {
-    var player = players[socket.id] || {};
-    if (data.left) {
-      player.x -= 5;
-    }
-    if (data.up) {
-      player.y -= 5;
-    }
-    if (data.right) {
-      player.x += 5;
-    }
-    if (data.down) {
-      player.y += 5;
-    }
+     console.log("disconnected " + socket);
   });
   
 }); //END OF SOCKET ON
--- public/game.js
+++ public/game.js
@@ -1,51 +1,15 @@
-console.log("in js")
 var socket = io();
-
-// var rule1 = document.getElementById("rule1");
-// var rule2 = document.getElementById("rule2");
-// var rule3 = document.getElementById("rule3");
-
-// var ruleList = document.getElementById("resultList");
-
-// document.getElementById("sendButton").addEventListener('click', function(){
-
-// 	var checkCompleted = checkRuleValue([rule1, rule2, rule3]);
-
-// 	if(checkCompleted !== ""){
-// 		document.getElementById("warning").innerHTML = checkCompleted;
-// 	} else {
-// 		document.getElementById("warning").innerHTML = "";
-// 		socket.emit('new rules', [rule1.value, rule2.value, rule3.value]);
-// 	}
-// });
-
-// socket.on('all rules', function(rules){
-// 	var ruleList = document.getElementById("resultList");
-	
-		
-
-// ruleList.innerHTML = "";
-// 		rules.forEach(function(rule, i){
-// 			var pTag = document.createElement('p');
-// 			if(i % 3 !== 2){
-// 				console.log(rule);
-// 				pTag.setAttribute('class',"blankedout");
-// 			}
-// 			pTag.innerText = rule;
-// 			ruleList.appendChild(pTag);
-// 		});
-
-// });
 
 document.getElementById("sendButton").addEventListener('click', function(){
 
-const elems = document.getElementsByClassName('name');
+const elems = document.getElementsByClassName('rule');
 const elemsLength = elems.length;
 
 var incompleteInput = "Please fill in  ";
+var completedInput = false;
 
 var ruleArray = [];
-var completedInput = false;
+
 
 for(i = 0; i < elemsLength; i++){
 	if(elems[i].value === ""){
@@ -56,72 +20,30 @@ for(i = 0; i < elemsLength; i++){
 		ruleArray.push(elems[i].value);
 		completedInput = true;
 	}
-
-
 }
 
 	if(completedInput === true){
 		document.getElementById("warning").innerHTML = "";
 		document.getElementById("sendButton").disabled = true;
-		socket.emit('new player', ruleArray);
+		socket.emit('new rules', ruleArray);
 	}
-
-// iterate using for...of loop
-// for (const p of elems) {
-    
-//     if(p.value === ""){
-//     	console.log(elems)
-//     }
-//     ruleArray.push(p.value);
-// }
-  
-  // if (name.length === 0){
-  //   name = "no name entered";
-  // }
-  //A socket.emit is sent to the server with the new players name
-  // socket.emit('new player', ruleArray);
-  
-  //Once the player has put in their name, the input field dissapears so they 
-  //Can't add another name and can only play one circle
-  // document.getElementById('inputName').style.display = 'none';
 })
   
-//The unordered list
-let list = document.getElementById("playersNames");
+
+
 let rulesList = document.getElementById("resultList");
 
-socket.on('update player names', function(newPlayerName){
+socket.on('update rules', function(newRule){
 
-  newPlayerName.forEach(function(name, index){
+  newRule.forEach(function(name, index){
   	let pNode = document.createElement("P");
   	let textnode = document.createTextNode(name);
   	if(index % 3 !== 2){
 		pNode.className = "blankedout";
 	}
-  	// pNode.className = "blankedout"
   	pNode.appendChild(textnode);
   	rulesList.appendChild(pNode)
   })
-  
 
 });
 
-
-function checkRuleValue(ruleArray){
-	var completedInput = "";
-	var incompleteInput = "Please fill in  ";
-	var rulesIncomplete = false;
-
-	ruleArray.forEach(function(rule, i){
-		if(rule.value === ""){
-			incompleteInput += "rule " + (i + 1) + " ";
-			rulesIncomplete = true;
-		}
-	})
-
-	if(rulesIncomplete){
-		return incompleteInput;
-	} else {
-		return completedInput;
-	}
-}
